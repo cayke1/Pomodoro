73a0a2415b66ef312308622e4f3bd966abd85a00
Cayke D <caykedanielcd@gmail.com>
2022-10-16 22:19:52 -0300

    Set inital config to styled-components

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
+import { Button } from "./comonents/Button";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
